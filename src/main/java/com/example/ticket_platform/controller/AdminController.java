95e124cf9ac29cdca30af08b46f8083dfd8c15ec
AggiusVS <aggiusvellastefano@gmail.com>
2025-10-17 19:00:07 +0200

    adminController, admin html

--- src/main/java/com/example/ticket_platform/controller/AdminController.java
+++ src/main/java/com/example/ticket_platform/controller/AdminController.java
@@ -6,13 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.ticket_platform.model.Note;
 import com.example.ticket_platform.model.Ticket;
+import com.example.ticket_platform.model.User;
+import com.example.ticket_platform.repository.CategoryRepository;
 import com.example.ticket_platform.repository.NoteRepository;
 import com.example.ticket_platform.repository.TicketRepository;
+import com.example.ticket_platform.repository.UserRepository;
+
+
 
 
 @Controller
@@ -24,6 +32,12 @@ public class AdminController {
 
     @Autowired
     private NoteRepository noteRepository;
+
+    @Autowired 
+    private UserRepository userRepository;
+
+    @Autowired 
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/tickets")
     public String listTickets(@RequestParam(required=false) String keyword, Model model) {
@@ -43,6 +57,43 @@ public class AdminController {
         Ticket ticket = ticketRepository.findById(id).orElseThrow();
         model.addAttribute("ticket", ticket);
         return "admin/ticket-detail";
+    }
+    
+    @PostMapping("/tickets/{id}/notes")
+    public String addNote(@PathVariable Long id, @RequestParam String content) {
+        Ticket ticket = ticketRepository.findById(id).orElseThrow();
+        Note note = new Note();
+        note.setTicket(ticket);
+        note.setContent(content);
+        noteRepository.save(note);
+        return "redirect:/admin/tickets/" + id;
+    }
+    
+    @GetMapping("/tickets/new")
+    public String newTicketForm(Model model) {
+        Ticket ticket = new Ticket();
+        model.addAttribute("ticket", ticket);
+        model.addAttribute("availableOperators", userRepository.findAll().stream()
+                            .filter(u -> !u.isUnavailable() && u.getRole() == User.Role.OPERATOR).toList());
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "admin/ticket-form";
+                            
+    }
+    
+    @PostMapping("/tickets")
+    public String saveNewTicket(@ModelAttribute Ticket ticket) {
+        ticketRepository.save(ticket);
+        return "redirect:/admin/tickets";
+    }
+    
+    @GetMapping("/tickets/{id}/edit")
+    public String editTicketForm(@PathVariable Long id, Model model) {
+        Ticket ticket = ticketRepository.findById(id).orElseThrow();
+        model.addAttribute("ticket", ticket);
+        model.addAttribute("availableOperators", userRepository.findAll().stream()
+                            .filter(u -> !u.isUnavailable() && u.getRole() == User.Role.OPERATOR).toList());
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "admin/ticket-form";
     }
     
 }
--- src/main/java/com/example/ticket_platform/repository/NoteRepository.java
+++ src/main/java/com/example/ticket_platform/repository/NoteRepository.java
@@ -1,9 +1,11 @@
 package com.example.ticket_platform.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ticket_platform.model.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-
+    List<Note> findByTicketId(Long ticketId);
 }
