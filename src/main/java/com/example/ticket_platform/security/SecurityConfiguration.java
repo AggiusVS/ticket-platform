af1a28e648d2cf30c587b874496e9d6b1b1a6a89
AggiusVS <aggiusvellastefano@gmail.com>
2025-10-21 17:17:28 +0200

    update security

--- src/main/java/com/example/ticket_platform/security/SecurityConfiguration.java
+++ src/main/java/com/example/ticket_platform/security/SecurityConfiguration.java
@@ -1,13 +1,23 @@
 package com.example.ticket_platform.security;
 
+import java.io.IOException;
+import java.util.Set;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfiguration {
@@ -15,23 +25,43 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-            .requestMatchers("/admin/**").hasRole("ADMIN")
-            .requestMatchers("/operator/**").hasRole("OPERATOR")
-            .requestMatchers(HttpMethod.GET, "/api/tickets/**").permitAll()
-            .anyRequest().authenticated()
-            .and().formLogin().defaultSuccessUrl("/admin/tickets", true)
-            .and().logout()
-            .and().csrf().disable();
-            return http.build();
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/operator/**").hasRole("OPERATOR")
+                .requestMatchers(HttpMethod.GET, "/api/tickets/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().successHandler(successHandler())
+                .and().logout()
+                .and().csrf().disable();
+        return http.build();
     }
-    
+
+     @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                    throws IOException, ServletException {
+
+                Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+                if (roles.contains("ROLE_ADMIN")) {
+                    response.sendRedirect("/admin/tickets");
+                } else if (roles.contains("ROLE_OPERATOR")) {
+                    response.sendRedirect("/operator/tickets");
+                } else {
+                    response.sendRedirect("/");
+                }
+            }
+        };
+    }
+
     @Bean
-    DatabaseUserDetailsService userDetailsService(){
+    DatabaseUserDetailsService userDetailsService() {
         return new DatabaseUserDetailsService();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
